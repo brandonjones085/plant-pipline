dce13caa56439f91f7475529016ec4a588f71eba
brandonjones085 <brandon.jones085@gmail.com>
2020-10-12 15:53:58 -0400

    testing pipeline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component'
+import { DayOneComponent } from './day-one/day-one.component'
+import { DayTwoComponent } from './day-two/day-two.component'
+import { DayThreeComponent } from './day-three/day-three.component'
+import { DayFourComponent} from './day-four/day-four.component'
+import { DayFiveComponent } from './day-five/day-five.component'
+import { CongratsComponent } from './congrats/congrats.component'
+import { GameOverComponent } from './game-over/game-over.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "", component: WelcomeComponent}, 
+  {path: "dayone", component: DayOneComponent}, 
+  {path: "daytwo", component: DayTwoComponent}, 
+  {path: "daythree", component: DayThreeComponent}, 
+  {path: "dayfour", component: DayFourComponent}, 
+  {path: "dayfive", component: DayFiveComponent}, 
+  {path: "congrats", component: CongratsComponent}, 
+  {path: "gameover", component: GameOverComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
